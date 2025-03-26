e18ba8d9c0f196c88ab4742f1bb31de171f92b0d
iambin6 <miraisan2512@gmail.com>
2025-03-26 11:21:33 +0700

    Delete SolarSystem directory

--- SolarSystem/build/classes/solarsystem/SolarSystem.rs
+++
@@ -1,9 +0,0 @@
-solarsystem.SolarSystemController
-solarsystem.MainMenu
-solarsystem.SolarSystemObjects
-solarsystem.SolarSystemView
-solarsystem.Node
-solarsystem.Menu
-solarsystem.AVLTree
-solarsystem.SolarSystem
-solarsystem.Validation
